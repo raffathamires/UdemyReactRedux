1e4d22ca9fa0081a7ba974fb2c9bfd04321a94d2
raffathamires <raffaela.monteiro@gmail.com>
2017-09-17 13:11:49 -0300

    Iniciando app8

---
+++ app8/index.android.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import {
+  AppRegistry
+} from 'react-native';
+
+import App from './src/App';
+
+const app8 = props => (
+  <App />
+);
+
+AppRegistry.registerComponent('app8', () => app8);
